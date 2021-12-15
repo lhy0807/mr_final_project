4782567e2fbdc7d8e880f21ae9b7fd0db42996e8
michael-shaham <michael.h.shaham@gmail.com>
2021-12-15 18:46:59 -0500

    simplifying changes for tracking code

--- nodes/tag_tracker.py
+++ nodes/tag_tracker.py
@@ -6,7 +6,7 @@ import tf2_geometry_msgs
 
 from apriltag_ros.msg import AprilTagDetectionArray
 from geometry_msgs.msg import TransformStamped
-from visualization_msgs.msg import MarkerArray, Marker
+from visualization_msgs.msg import Marker
 
 class TagsUpdater:
 
@@ -22,8 +22,7 @@ class TagsUpdater:
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-        self.vis_pub = rospy.Publisher("visualization_marker", Marker, \
-            queue_size=1)
+
         while True:
             try:
                 _ = self.tf_buffer.lookup_transform('map', 'base_link', \
@@ -33,71 +32,36 @@ class TagsUpdater:
                     tf2_ros.ExtrapolationException):
                 continue
 
-
     def tags_cb(self, tags):
         # iterate through tags
         for tag in tags.detections:
             # Get tag info
             tag_id = tag.id[0]
             tag_name = 'map_tag_' + str(tag_id)
-            frame_tag_pose = tag.pose.pose.pose
-            frame = tag.pose.header.frame_id
+            frame_tag_tf = tf2_geometry_msgs.PoseStamped()
+            frame_tag_tf.pose = tag.pose.pose.pose
+            frame_tag_tf.header = tag.pose.header
+            try:
+                map_tag_tf = self.tf_buffer.transform(frame_tag_tf, "map")
+            except:
+                pass
 
-            # Get transform from map to tag
-            # map_frame_transform = self.tf_buffer.lookupTransform(\
-            #     '/map', '/' + frame, rospy.Time(0))
-            map_tag_pose = self.transform_pose(frame_tag_pose, frame, 'map')
-
-            self.tag_estimates[tag_id] = map_tag_pose
-            print(map_tag_pose)
-        for i, est in enumerate(self.tag_estimates):
-            if est:
-                t = TransformStamped()
-                t.header.stamp = rospy.Time.now()
-                t.header.frame_id = 'map'
-
-                t.child_frame_id = 'map_tag_' + str(i)
-                t.transform.translation.x = self.tag_estimates[i].position.z
-                t.transform.translation.y = self.tag_estimates[i].position.x
-                t.transform.translation.z = self.tag_estimates[i].position.y
-                t.transform.rotation = self.tag_estimates[i].orientation
-                self.tf_broadcaster.sendTransform(t)
-
-                marker = Marker()
-                marker.header.frame_id = "map"
-                marker.ns = "our_tag"
-                marker.header.stamp = rospy.Time.now()
-                marker.id = i
-                marker.type = marker.SPHERE
-                marker.action = marker.ADD
-                marker.pose.position.x = self.tag_estimates[i].position.z
-                marker.pose.position.y = self.tag_estimates[i].position.x
-                marker.pose.position.z = self.tag_estimates[i].position.y
-                marker.pose.orientation = self.tag_estimates[i].orientation
-                marker.scale.x = 0.10
-                marker.scale.y = 0.10
-                marker.scale.z = 0.10
-                marker.color.a = 1.0
-                marker.color.r = 0.0
-                marker.color.g = 1.0
-                marker.color.b = 0.0
-                self.vis_pub.publish(marker)
-
-
-    def transform_pose(self, input_pose, init_frame, final_frame):
-        # **Assuming /tf2 topic is being broadcasted
-        pose_stamped = tf2_geometry_msgs.PoseStamped()
-        pose_stamped.pose = input_pose
-        pose_stamped.header.frame_id = init_frame
-        pose_stamped.header.stamp = rospy.Time.now()
-        try:
-            # ** It is important to wait for the listener to start listening. Hence the rospy.Duration(1)
-            output_pose_stamped = self.tf_buffer.transform(pose_stamped, \
-                final_frame, rospy.Duration(1))
-            return output_pose_stamped.pose
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, \
-            tf2_ros.ExtrapolationException):
-            pass
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.ns = tag_name
+            marker.header.stamp = rospy.Time.now()
+            marker.id = 0
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+            marker.pose = map_tag_tf.pose
+            marker.scale.x = 0.25
+            marker.scale.y = 0.25
+            marker.scale.z = 0.25
+            marker.color.a = 1.0
+            marker.color.r = 0.0
+            marker.color.b = 1.0
+            marker.color.g = 0.0
+            self.vis_pub.publish(marker)
 
 if __name__ == "__main__":
     rospy.init_node('tag_updater')
