af3c35d761586b038d8114e7eca31518e2166b25
michael-shaham <michael.h.shaham@gmail.com>
2021-12-14 19:31:08 -0500

    first draft

--- nodes/tag_tracker.py
+++ nodes/tag_tracker.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import rospy
-import tf
+import tf2_ros
 from apriltag_ros.msg import AprilTagDetectionArray
-from geometry_msgs.msg import Pose
-
+from geometry_msgs.msg import Pose, TransformStamped
+import tf2_geometry_msgs
 
 class TagsUpdater:
 
@@ -13,19 +13,59 @@ class TagsUpdater:
         self.tag_estimates = [None for _ in range(num_tags)]
         self.tags_sub = rospy.Subscriber("tag_detections", \
             AprilTagDetectionArray, self.tags_cb)
-        self.tf_broadcaster = tf.TransformBroadcaster()
-        self.tf_listener = tf.TransformListener()
-        
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        while True:
+            try:
+                _ = self.tf_buffer.lookupTransform('/map', '/base_link', \
+                    rospy.Time(0))
+                break
+            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, \
+                    tf2_ros.ExtrapolationException):
+                continue
 
-    def tags_cb(tags):
+
+    def tags_cb(self, tags):
         # iterate through tags
         for tag in tags:
+            # Get tag info
             tag_id = tag.id[0]
-            tag_name = 'global_tag_' + str(tag_id)
-            local_pose = tag.pose.pose.pose
+            tag_name = 'map_tag_' + str(tag_id)
+            frame_tag_pose = tag.pose.pose.pose
             frame = tag.pose.frame_id
-            (trans, rot) = 
 
+            # Get transform from map to tag
+            # map_frame_transform = self.tf_buffer.lookupTransform(\
+            #     '/map', '/' + frame, rospy.Time(0))
+            map_tag_pose = self.transform_pose(frame_tag_pose, frame, 'map')
+            self.tag_estimates[tag_id] = map_tag_pose
+        
+        for i, est in enumerate(self.tag_estimates):
+            if not est:
+                t = TransformStamped()
+                t.header.stamp = rospy.Time.now()
+                t.header.frame_id = 'map'
+                t.child_frame_id = tag_name
+                t.transform.translation = self.tag_estimates[i].translation
+                t.transform.translation = self.tag_estimates[i].rotation
+                self.tf_broadcaster.sendTransform(t)
+
+
+    def transform_pose(self, input_pose, init_frame, final_frame):
+        # **Assuming /tf2 topic is being broadcasted
+        pose_stamped = tf2_geometry_msgs.PoseStamped()
+        pose_stamped.pose = input_pose
+        pose_stamped.header.frame_id = init_frame
+        pose_stamped.header.stamp = rospy.Time.now()
+        try:
+            # ** It is important to wait for the listener to start listening. Hence the rospy.Duration(1)
+            output_pose_stamped = self.tf_buffer.transform(pose_stamped, \
+                final_frame, rospy.Duration(1))
+            return output_pose_stamped.pose
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, \
+            tf2_ros.ExtrapolationException):
+            pass
 
 if __name__ == "__main__":
     rospy.init_node('tag_updater')
