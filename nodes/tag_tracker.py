70bc9e65674234bbf9b180418cda47cc88b1710b
michael-shaham <michael.h.shaham@gmail.com>
2021-12-15 16:04:37 -0500

    tag track and exploration working decently

--- nodes/tag_tracker.py
+++ nodes/tag_tracker.py
@@ -6,14 +6,19 @@ import tf2_geometry_msgs
 
 from apriltag_ros.msg import AprilTagDetectionArray
 from geometry_msgs.msg import TransformStamped
+from visualization_msgs.msg import Marker
 
 class TagsUpdater:
 
     def __init__(self):
+        rospy.sleep(1.)
         num_tags = 20
         self.tag_estimates = [None for _ in range(num_tags)]
         self.tags_sub = rospy.Subscriber("tag_detections", \
             AprilTagDetectionArray, self.tags_cb)
+        self.vis_pub = rospy.Publisher("visualization_marker", Marker, \
+            queue_size=1)
+
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
@@ -40,18 +45,36 @@ class TagsUpdater:
             # map_frame_transform = self.tf_buffer.lookupTransform(\
             #     '/map', '/' + frame, rospy.Time(0))
             map_tag_pose = self.transform_pose(frame_tag_pose, frame, 'map')
-            self.tag_estimates[tag_id] = map_tag_pose
+            self.tag_estimates[tag_id] = (tag_name, map_tag_pose)
+
+            # visualize tag location
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.ns = tag_name
+            marker.header.stamp = rospy.Time.now()
+            marker.id = 0
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+            marker.pose = map_tag_pose
+            print(marker.pose)
+            marker.scale.x = 0.25
+            marker.scale.y = 0.25
+            marker.scale.z = 0.25
+            marker.color.a = 1.0
+            marker.color.b = 1.0
+            self.vis_pub.publish(marker)
+
         
         for i, est in enumerate(self.tag_estimates):
             if est:
                 t = TransformStamped()
                 t.header.stamp = rospy.Time.now()
                 t.header.frame_id = 'map'
-                t.child_frame_id = tag_name
-                t.transform.translation.x = self.tag_estimates[i].position.x
-                t.transform.translation.y = self.tag_estimates[i].position.y
-                t.transform.translation.z = self.tag_estimates[i].position.z
-                t.transform.translation = self.tag_estimates[i].orientation
+                t.child_frame_id = est[0]
+                t.transform.translation.x = est[1].position.x
+                t.transform.translation.y = est[1].position.y
+                t.transform.translation.z = est[1].position.z
+                t.transform.translation = est[1].orientation
                 self.tf_broadcaster.sendTransform(t)
 
 
