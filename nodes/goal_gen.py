85d98adb7750e0c27f7900a85845478a04200a46
Mingxi Jia <jiadanny@outlook.com>
2021-12-15 15:44:22 -0500

    visualize tags as markers

--- nodes/goal_gen.py
+++ nodes/goal_gen.py
@@ -4,46 +4,133 @@ import numpy as np
 import rospy
 import actionlib
 
+from geometry_msgs.msg import Twist
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from nav_msgs.msg import OccupancyGrid
-from visualization_msgs.msg import MarkerArray
+from visualization_msgs.msg import MarkerArray, Marker
 
 class GoalGen:
 
     def __init__(self):
-        self.frontier_sub = rospy.Subscriber("/explore/frontiers", \
-            MarkerArray, self.frontier_cb)
-        self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb)
-        self.move_base_client = actionlib.SimpleActionClient('move_base', \
-            MoveBaseAction)
-        self.move_base_client.wait_for_server()
-        self.frontier_empty = False
+        # map data
         self.map_resolution = None
         self.map_width = None
         self.map_height = None
         self.map_origin = None
         self.map = None
 
-    def frontier_cb(self, data):
-        if not self.frontier_empty:
-            if len(data.markers.points) == 0:
-                self.frontier_empty = True
-        else:
-            if not self.map:  # make sure we have a map
-                # generate goals
-                pass
-    
-    def map_cb(self, occ_grid):
-        if self.frontier_empty:
-            self.map_resolution = occ_grid.info.resolution
-            self.map_width = occ_grid.info.width
-            self.map_height = occ_grid.info.height
-            self.map_origin = occ_grid.info.origin
-            self.map = np.array(occ_grid.data).reshape((self.map_height, \
-                                                        self.map_width))
+        # frontier data (used to decide when to send random commands)
+        self.last_frontier_msg_time = rospy.get_time()
+        self.time_since_last_frontier_msg = -1
+        self.time_diff = 5.0
 
-    def send_random_goal(self):
-        pass
+        # pubs and subs
+        rospy.sleep(1.)
+        self.cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        self.frontier_sub = rospy.Subscriber("/explore/frontiers", \
+            MarkerArray, self.frontier_cb)
+        self.map_sub = rospy.Subscriber("/map", OccupancyGrid, self.map_cb)
+        self.move_base_client = actionlib.SimpleActionClient('move_base', \
+            MoveBaseAction)
+        self.move_base_client.wait_for_server()
+        self.vis_pub = rospy.Publisher("visualization_marker", Marker, \
+            queue_size=1)
+
+        # message to send when we want to spin
+        self.spin_msg = Twist()
+        self.spin_msg.angular.z = 1.0
+        
+        
+    def frontier_cb(self, data):
+        if data:
+            self.last_frontier_msg_time = rospy.get_time()
+            
+    def map_cb(self, occ_grid):
+        self.map_resolution = occ_grid.info.resolution
+        self.map_width = occ_grid.info.width
+        self.map_height = occ_grid.info.height
+        self.map_origin = occ_grid.info.origin
+        self.map = np.array(occ_grid.data).reshape((self.map_height, \
+                                                    self.map_width))
+        
+        self.time_since_last_frontier_msg = rospy.get_time() - \
+            self.last_frontier_msg_time
+
+        if self.time_since_last_frontier_msg > self.time_diff:
+            if self.go_to_random_goal():
+                rospy.loginfo("Sent goal!")
+            else: 
+                rospy.loginfo("Didn't send goal or action didn't work!")
+
+    def go_to_random_goal(self):
+        while True:
+            # get random index for map
+            rand_h = np.random.randint(0, self.map_height)
+            rand_w = np.random.randint(0, self.map_width)
+
+            # ignore positions too close to obstacles
+            n = round(.3 / self.map_resolution)
+            min_h = max(0, rand_h - n)
+            max_h = min(self.map_height, rand_h + n)
+            min_w = max(0, rand_w - n)
+            max_w = min(self.map_width, rand_w + n)
+            resample_flag = False
+            for i in range(min_h, max_h):
+                for j in range(min_w, max_w):
+                    if self.map[i, j] == -1 or self.map[i, j] >= 80:
+                        resample_flag = True
+                    if resample_flag:
+                        break
+                if resample_flag:
+                    break
+            if resample_flag:
+                continue
+
+
+            # make sure location in map is likely unoccupied
+            if self.map[rand_h, rand_w] >= 0 and self.map[rand_h, rand_w] <= 10:
+                new_x = self.map_origin.position.x + \
+                    rand_h * self.map_resolution
+                new_y = self.map_origin.position.y + \
+                    rand_w * self.map_resolution
+                goal = MoveBaseGoal()
+                goal.target_pose.header.frame_id = "map"
+                goal.target_pose.header.stamp = rospy.Time.now()
+                goal.target_pose.pose.position.x = new_x
+                goal.target_pose.pose.position.y = new_y
+                goal.target_pose.pose.orientation.w = 1.0
+
+                # visualize marker
+                marker = Marker()
+                marker.header.frame_id = "map"
+                marker.ns = "goal"
+                marker.header.stamp = rospy.Time.now()
+                marker.id = 0
+                marker.type = marker.SPHERE
+                marker.action = marker.ADD
+                marker.pose = goal.target_pose.pose
+                marker.scale.x = 0.25
+                marker.scale.y = 0.25
+                marker.scale.z = 0.25
+                marker.color.a = 1.0
+                marker.color.r = 1.0
+                self.vis_pub.publish(marker)
+
+                self.move_base_client.send_goal(goal)
+                wait = self.move_base_client.wait_for_result()
+                if not wait:
+                    rospy.logerr("Goal not sent!")
+                    return False
+                success = self.move_base_client.get_result()
+
+                # spin in circle for a brief period of time
+                start_time = rospy.get_time()
+                while rospy.get_time() - start_time < 7.0:
+                    self.cmd_vel_pub.publish(self.spin_msg)
+                return success
+
+
+
 
 if __name__ == "__main__":
     rospy.init_node("goal_gen")
--- nodes/tag_tracker.py
+++ nodes/tag_tracker.py
@@ -6,6 +6,8 @@ import tf2_geometry_msgs
 
 from apriltag_ros.msg import AprilTagDetectionArray
 from geometry_msgs.msg import TransformStamped
+from visualization_msgs.msg import MarkerArray, Marker
+
 
 class TagsUpdater:
 
@@ -17,6 +19,8 @@ class TagsUpdater:
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.vis_pub = rospy.Publisher("visualization_marker", Marker, \
+            queue_size=1)
         while True:
             try:
                 _ = self.tf_buffer.lookup_transform('map', 'base_link', \
@@ -41,18 +45,38 @@ class TagsUpdater:
             #     '/map', '/' + frame, rospy.Time(0))
             map_tag_pose = self.transform_pose(frame_tag_pose, frame, 'map')
             self.tag_estimates[tag_id] = map_tag_pose
-        
+            print(map_tag_pose)
         for i, est in enumerate(self.tag_estimates):
             if est:
                 t = TransformStamped()
                 t.header.stamp = rospy.Time.now()
                 t.header.frame_id = 'map'
                 t.child_frame_id = 'map_tag_' + str(i)
-                t.transform.translation.x = self.tag_estimates[i].position.x
-                t.transform.translation.y = self.tag_estimates[i].position.y
-                t.transform.translation.z = self.tag_estimates[i].position.z
+                t.transform.translation.x = self.tag_estimates[i].position.z
+                t.transform.translation.y = self.tag_estimates[i].position.x
+                t.transform.translation.z = self.tag_estimates[i].position.y
                 t.transform.rotation = self.tag_estimates[i].orientation
                 self.tf_broadcaster.sendTransform(t)
+
+                marker = Marker()
+                marker.header.frame_id = "map"
+                marker.ns = "our_tag"
+                marker.header.stamp = rospy.Time.now()
+                marker.id = i
+                marker.type = marker.SPHERE
+                marker.action = marker.ADD
+                marker.pose.position.x = self.tag_estimates[i].position.z
+                marker.pose.position.y = self.tag_estimates[i].position.x
+                marker.pose.position.z = self.tag_estimates[i].position.y
+                marker.pose.orientation = self.tag_estimates[i].orientation
+                marker.scale.x = 0.10
+                marker.scale.y = 0.10
+                marker.scale.z = 0.10
+                marker.color.a = 1.0
+                marker.color.r = 0.0
+                marker.color.g = 1.0
+                marker.color.b = 0.0
+                self.vis_pub.publish(marker)
 
 
     def transform_pose(self, input_pose, init_frame, final_frame):
